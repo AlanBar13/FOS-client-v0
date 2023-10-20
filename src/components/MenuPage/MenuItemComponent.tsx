2447efed8a72e7725598f9d7391d2cf2a8056363
Alan Bardales <alan.g.bardales@gmail.com>
2023-10-20 11:36:34 -0600

    Added cart drawer

--- src/components/MenuPage/MenuItemComponent.tsx
+++ src/components/MenuPage/MenuItemComponent.tsx
@@ -1,15 +1,45 @@
+import { useState } from 'react'
 import { Menu } from '../../models/Menu';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 
 interface MenuItemComponentProps {
     item: Menu
+    onAddClicked: (item: Menu, qty: number) => void
 }
 
-export default function MenuItemComponent({ item }: MenuItemComponentProps){
+export default function MenuItemComponent({ item, onAddClicked }: MenuItemComponentProps){
+    const [qty, setQty] = useState(1);
+
+    const handleChange = (operation: string) => {
+        if (operation === "-") {
+            const value = qty - 1;
+            if (value < 1){
+                setQty(1);
+                return;
+            }
+
+            setQty(value);
+            return;
+        } else {
+            //TODO: allow user to set max.
+            const value = qty + 1;
+            if (value > 10){
+                setQty(10);
+                return;
+            }
+
+            setQty(value);
+            return;
+        }
+    }
+
     return (
         <Card sx={{ display: 'flex', marginBottom: '1rem' }}>
             {item.img !== null ? (
@@ -20,7 +50,7 @@ export default function MenuItemComponent({ item }: MenuItemComponentProps){
                     alt={item.name}
                 />
             ) : ''}
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
                 <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography component="div" variant="h5">
                         {item.name}
@@ -29,10 +59,14 @@ export default function MenuItemComponent({ item }: MenuItemComponentProps){
                         {item.description}
                     </Typography>
                 </CardContent>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography color="text.secondary" component="div">
-                        Mac Miller
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginBottom: '0.3rem'}}>
+                    <Typography sx={{marginRight: '0.3rem'}} component="span" variant="caption">
+                        max. 10
                     </Typography>
+                    <IconButton size='large' onClick={() => handleChange("-")}>-</IconButton>
+                    <TextField sx={{width: '2.9rem', alignItems: 'flex-end'}} inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', readOnly: true }} value={qty} />
+                    <IconButton size='large' onClick={() => handleChange("+")}>+</IconButton>
+                    <Button sx={{marginLeft: '0.3rem'}} onClick={() => onAddClicked(item, qty)}>Añadir</Button>
                 </Box>
             </Box>
         </Card>
--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,21 +1,46 @@
 import {useState, useEffect, SyntheticEvent} from 'react';
 import { Menu } from '../models/Menu';
 import { fetchMenu } from '../services/menu.service';
+import { useQuery } from '../hooks/useQuery';
 
+import { Global } from '@emotion/react';
+import { styled } from '@mui/material/styles';
 import LinearProgress from '@mui/material/LinearProgress';
 import Snackbar from '@mui/material/Snackbar';
 import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Typography from '@mui/material/Typography';
 
 import AlertComponent from '../components/Shared/AlertComponent';
 import MenuItemComponent from '../components/MenuPage/MenuItemComponent';
 import AppLayout from '../components/Shared/AppLayout';
 
+const drawerBleeding = 56;
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'light' ? '#fff' : 'grey',
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === 'light' ? 'grey' : 'white',
+    borderRadius: 3,
+    position: 'absolute',
+    top: 8,
+    left: 'calc(50% - 15px)',
+}));
+
 export default function MenuPage(){
     const [companyName, _] = useState<string>(import.meta.env.VITE_COMPANY_NAME);
     const [menu, setMenu] = useState<Menu[]>([]);
     const [open, setOpen] = useState<boolean>(false);
+    const [openCart, setOpenCart] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [cartQty, setCartQty] = useState<number>(0);
+
+    const query = useQuery();
 
     const handleClose = (_?: SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
@@ -42,12 +67,67 @@ export default function MenuPage(){
         fetchData();
     }, []);
 
+    const addToCart = (item: Menu, qty: number) => {
+        console.log(qty, item);
+        setCartQty(cartQty + 1);
+        setOpenCart(true);
+    }
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpenCart(newOpen);
+    };
+
     return (
         <AppLayout companyName={`${companyName} | Menu`}>
+            <Global
+                styles={{
+                '.MuiDrawer-root > .MuiPaper-root': {
+                    height: `calc(50% - ${drawerBleeding}px)`,
+                    overflow: 'visible',
+                },
+                }}
+            />
             {isLoading && <LinearProgress />}
             <Box sx={{marginTop: '0.5rem', marginLeft: '0.5rem', marginRight: '0.5rem'}}>
-                {menu.map((item) => <MenuItemComponent key={item.id} item={item} />)}
+                {menu.map((item) => <MenuItemComponent key={item.id} item={item} onAddClicked={addToCart} />)}
             </Box>
+            <SwipeableDrawer 
+                anchor='bottom' 
+                open={openCart} 
+                onClose={toggleDrawer(false)} 
+                onOpen={toggleDrawer(true)} 
+                swipeAreaWidth={drawerBleeding} 
+                disableSwipeToOpen={false} 
+                ModalProps={{keepMounted: true}}
+            >
+                <StyledBox
+                    sx={{
+                        position: 'absolute',
+                        top: -drawerBleeding,
+                        borderTopLeftRadius: 8,
+                        borderTopRightRadius: 8,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0,
+                        background: '#FFC107'
+                    }}
+                >
+                    <Puller />
+                    <Typography sx={{ p: 2, color: 'text.secondary.contrastText' }}>
+                        {query.get("mesa") !== null ? `Mesa ${query.get("mesa")}` : `No hay mesa seleccionada`} | Carrito ({cartQty})
+                    </Typography>
+                </StyledBox>
+                <StyledBox
+                    sx={{
+                        px: 2,
+                        pb: 2,
+                        height: '100%',
+                        overflow: 'auto',
+                    }}
+                    >
+                    Hello
+                </StyledBox>
+            </SwipeableDrawer>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
                 <AlertComponent severity='error'>Error: {error}</AlertComponent>
             </Snackbar>
